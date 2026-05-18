95750697ec0f94534d985d9e8337a42b6d62999a
Zephyr308 <87972042+Zephyr308@users.noreply.github.com>
2026-05-18 18:27:27 +0300

    Create config.py

---
+++ IoT/vehicle-tracking-system/edge-device/config.py
@@ -0,0 +1,10 @@
+MQTT_BROKER = "broker.hivemq.com"
+MQTT_PORT = 1883
+MQTT_TOPIC = "vehicle/livegps"
+
+DEVICE_ID = "vehicle_001"
+
+DATABASE = "gps_data.db"
+
+GEOCODE_INTERVAL = 60
+PUBLISH_INTERVAL = 1
